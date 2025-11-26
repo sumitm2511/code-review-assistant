40fec882c1161c120e5d2e445f09c89e9c9ded02
sumitm2511 <sumitmeena2529@gmail.com>
2025-11-26 17:04:28 +0530

    Error

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,5 @@
-// src/api/client.js
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api", // change if backend runs elsewhere
-  timeout: 60_000,
+  baseURL: "https://code-review-backend-4ohe.onrender.com/api",  
 });
